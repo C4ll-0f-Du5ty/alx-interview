5b2c19870c49bd2322410b1fd255e7aa15222030
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-24 13:55:02 +0300

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -27,14 +27,14 @@ patt = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] "(.*?)" (\d+) (\d+)'
 counter = 0
 size = 0
 statusCode = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0,
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0,
 }
 
 
@@ -44,7 +44,7 @@ try:
         match = re.match(patt, line)
         if match:
             size += int(match.group(5))
-            statusCode[int(match.group(4))] += 1
+            statusCode[match.group(4)] += 1
             counter += 1
             if counter % 10 == 0:
                 print(f"File size: ", size)
