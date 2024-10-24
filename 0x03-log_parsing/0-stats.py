e4f5e177792ee4fc8002bcef577a5832c66c0d0d
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-24 13:24:52 +0300

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,11 +1,56 @@
 #!/usr/bin/python3
 
+import signal
 import re
 import sys
 
-pattern = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] "(.*?)" (\d+) (\d+)'
+patt = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] "(.*?)" (\d+) (\d+)'
+counter = 0
+size = 0
+statusCode = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0,
+}
+
+
+def signal_handler(sig, frame):
+    global finished
+    finished = True
+    print(f"File size: {size}")
+    for key, value in statusCode.items():
+        if value > 0:
+            print(f"{key}: {value}")
+
+
+finished = False
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
 
 for line in sys.stdin:
-    match = re.match(pattern, line)
-    if match:
-        print(match)
+    try:
+        match = re.match(patt, line)
+        if match:
+            size += int(match.group(5))
+            statusCode[int(match.group(4))] += 1
+            counter += 1
+            if counter % 10 == 0:
+                print(f"File size: ", size)
+                for key, value in statusCode.items():
+                    if value > 0:
+                        print(f"{key}: {value}")
+    except KeyboardInterrupt:
+        pass
+
+if not finished:
+    print(f"File size: {size}")
+    for key, value in statusCode.items():
+        if value > 0:
+            print(f"{key}: {value}")
