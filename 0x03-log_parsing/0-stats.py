94956f8eb7103dbec5cbcac3dd6c37d8d5042c84
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-24 13:53:25 +0300

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
+"""Write a script that reads stdin line by line and computes metrics:
 
-import signal
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size> (if the format is not this one, the line
+must be skipped)
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+Total file size: File size: <total size>
+where <total size> is the sum of all previous <file size>
+(see input format above)
+Number of lines by status code:
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+if a status code doesn’t appear or is not an integer,
+don’t print anything for this status code
+format: <status code>: <number>
+status codes should be printed in ascending order
+
+line list = [<IP Address>, -, [<date>], "GET /projects/260 HTTP/1.1",
+<status code>, <file size>]
+"""
+
 import re
 import sys
 
@@ -19,17 +38,9 @@ statusCode = {
 }
 
 
-def signal_handler(sig, frame):
-    print(f"File size: {size}")
-    for key, value in statusCode.items():
-        if value > 0:
-            print(f"{key}: {value}")
+try:
+    for line in sys.stdin:
 
-
-signal.signal(signal.SIGINT, signal_handler)
-
-for line in sys.stdin:
-    try:
         match = re.match(patt, line)
         if match:
             size += int(match.group(5))
@@ -37,8 +48,13 @@ for line in sys.stdin:
             counter += 1
             if counter % 10 == 0:
                 print(f"File size: ", size)
-                for key, value in statusCode.items():
+                for key, value in sorted(statusCode.items()):
                     if value > 0:
                         print(f"{key}: {value}")
-    except KeyboardInterrupt:
-        pass
+except KeyboardInterrupt:
+    pass
+finally:
+    print(f"File size: {size}")
+    for key, value in statusCode.items():
+        if value > 0:
+            print(f"{key}: {value}")
